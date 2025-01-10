ce5e41e77b618e1f3746221d6b15d8b72fa2461d
ayou985 <chelhayoub6@gmail.com>
2025-01-10 16:43:10 +0100

    maj

--- src/Views/home.view.php
+++ src/Views/home.view.php
@@ -4,4 +4,10 @@ require_once (__DIR__ . "/partials/head.php");
 
 ?>
 
-<h1>Hello</h1>
+
+
+<?php 
+
+require_once (__DIR__ . "/partials/footer.php");
+
+?>
--- src/Views/partials/head.php
+++ src/Views/partials/head.php
@@ -5,19 +5,19 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>
-    
+
   </title>
   <script src="https://kit.fontawesome.com/f5a1d28d53.js" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="/public/css/style.css">
+  <link rel="stylesheet" href="/public/style/style.css">
 </head>
 
 <body>
   <nav class="navbar navbar-expand-lg myNav">
     <div class="container-fluid">
       <a class="navbar-brand" href="/">
-        <img src="/public/img/logo.png" alt="Logo" width="50" height="50"> Task'n'Kids
+        <img src="/public/img/logo.png" alt="Logo" width="50" height="50">
       </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -25,10 +25,50 @@
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link" href="/">Inscription</a>
+            <a class="nav-link" href="/">Accueil</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/">Produit</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/">A propos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/">Contact</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/"><img src="/public/img/image1.png" /></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/"><img src="/public/img/utilisateur.png" /></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/"><img src="/public/img/panier.png" /></a>
           </li>
         </ul>
       </div>
     </div>
   </nav>
+
+  <div id="carouselExampleControlsNoTouching" class="carousel slide" data-bs-touch="false">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="/public/img/carrousel1.png" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="..." class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="..." class="d-block w-100" alt="...">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
   <div class="myBody">
