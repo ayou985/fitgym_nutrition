bb17378515fdd06fc2483a4971d80f85c9847b0e
ayou985 <chelhayoub6@gmail.com>
2025-01-16 16:41:09 +0100

    maj

--- src/Views/partials/head.php
+++ src/Views/partials/head.php
@@ -4,9 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>
-
-  </title>
+  <title>FitGym nutrition</title>
   <script src="https://kit.fontawesome.com/f5a1d28d53.js" crossorigin="anonymous"></script>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
@@ -14,61 +12,67 @@
 </head>
 
 <body>
-  <nav class="navbar navbar-expand-lg myNav">
+<nav class="navbar navbar-expand-lg bg-body-black">
     <div class="container-fluid">
-      <a class="navbar-brand" href="/">
-        <img src="/public/img/logo.png" alt="Logo" width="50" height="50">
-      </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <img src="/public/img/logo.png" alt="logo" width="100" height="100">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" href="/">Accueil</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/">Produit</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/">A propos</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/">Contact</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/"><img src="/public/img/image1.png" /></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/"><img src="/public/img/utilisateur.png" /></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/"><img src="/public/img/panier.png" /></a>
-          </li>
-        </ul>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link" href="#">Accueil</a>
+          <a class="nav-link" href="#">Produits</a>
+          <a class="nav-link" href="#">À propos</a>
+          <a class="nav-link" href="#">Contact</a>
+        </div>
+        <div class="  navbar-icon ">
+          <ul class="nav-container">
+            <li class="nav-icon">
+              <i class="fa-solid fa-magnifying-glass"></i>
+            </li>
+            <li class="nav-icon">
+              <i class="fa-solid fa-user"></i>
+            </li>
+            <li class="nav-icon">
+              <i class="fa-solid fa-cart-shopping"></i>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </nav>
 
-  <div id="carouselExampleControlsNoTouching" class="carousel slide" data-bs-touch="false">
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="/public/img/carrousel1.png" class="d-block w-100" alt="...">
+  <div id="carouselExampleIndicators" class="carousel slide">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
       </div>
-      <div class="carousel-item">
-        <img src="..." class="d-block w-100" alt="...">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="/public/img/carrousel1.png" class="d-block w-100 " alt="carrousel">
+        </div>
+        <div class="carousel-item">
+          <img src="/public/img/carrousel2.jpg" class="d-block w-100" alt="carrousel">
+        </div>
+        <div class="carousel-item">
+          <img src="/public/img/carrousel3.jpg" class="d-block w-100" alt="carrousel">
+        </div>
       </div>
-      <div class="carousel-item">
-        <img src="..." class="d-block w-100" alt="...">
-      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
-  </div>
+    
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
+  <script src="https://kit.fontawesome.com/f5a1d28d53.js" crossorigin="anonymous"></script>
+
   <div class="myBody">
